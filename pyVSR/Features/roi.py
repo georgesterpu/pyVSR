b5885715a8a08af5939687a6610da09e5fee526a
John Tukey <george.sterpu@gmail.com>
2018-02-22 20:49:05 +0000

    added option to turn off alignment, small bug fix

--- pyVSR/Features/roi.py
+++ pyVSR/Features/roi.py
@@ -47,6 +47,11 @@ class ROIFeature(Feature):
         self._xres = extract_opts['window_size'][0]
         self._yres = extract_opts['window_size'][1]
 
+        if 'align' in extract_opts:
+            self._align = extract_opts['align']
+        else:
+            self._align = True
+
         if 'color' in extract_opts:
             self._channels = 3 if extract_opts['color'] is True else 1
 
@@ -81,13 +86,13 @@ class ROIFeature(Feature):
         self._preload_dlib_detector_fitter()
         roi_sequence = self.extract_roi_sequence(file)
         outfile = utils.file_to_feature(file, extension='.h5', tree_leaves=self._tree_leaves)
-        self._write_sequence_to_file(outfile, roi_sequence, 'gray', (None, None, None, None))
+        self._write_sequence_to_file(outfile, roi_sequence, 'pixels', (None, None, None, None))
 
     def extract_roi_sequence(self, file):
         stream = cv2.VideoCapture(file)
         vidframes = int(stream.get(cv2.CAP_PROP_FRAME_COUNT))
 
-        roi_seq = np.zeros((vidframes, self._xres, self._yres, self._channels), dtype=np.float32)
+        roi_seq = np.zeros((vidframes, self._yres, self._xres, self._channels), dtype=np.float32)
 
         current_frame = 0
 
@@ -104,28 +109,34 @@ class ROIFeature(Feature):
 
                 bbox = detections[0]
                 left, top, right, bottom = _get_bbox_corners(bbox, self._gpu)
-                face_area = dlib.rectangle(left, top, right, bottom)
-                landmarks5 = self._fitter5(frame, face_area)
 
-                aligned_face = dlib.get_face_chip(frame, landmarks5, 256)
-                aligned_face = np.asarray(aligned_face)
+                if self._align is True:
+                    face_coords = dlib.rectangle(left, top, right, bottom)
+                    landmarks5 = self._fitter5(frame, face_coords)
 
-                face_chip_area = dlib.rectangle(0, 0, aligned_face.shape[0], aligned_face.shape[1])
-                landmarks68 = self._fitter68(aligned_face, face_chip_area)
+                    face_img = dlib.get_face_chip(frame, landmarks5, 256)
+                    face_img = np.asarray(face_img)
+
+                else:
+
+                    face_img = frame[top:bottom, left:right]
+                    face_img = cv2.resize(face_img, (256, 256), interpolation=cv2.INTER_AREA)
+
+                face_chip_area = dlib.rectangle(0, 0, face_img.shape[0], face_img.shape[1])
+                landmarks68 = self._fitter68(face_img, face_chip_area)
 
                 arr = _dlib_parts_to_numpy(landmarks68)[48:68]
-                top_left, bottom_right = _get_array_bounds(arr, aligned_face.shape, border=self._border)
+                top_left, bottom_right = _get_array_bounds(arr, face_img.shape, border=self._border)
 
-                mouth_crop = aligned_face[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0], :]
-                mouth_crop = cv2.resize(mouth_crop, (self._xres, self._yres), cv2.INTER_CUBIC)
+                mouth_crop = face_img[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0], :]
+                mouth_crop_resized = cv2.resize(mouth_crop, (self._xres, self._yres), cv2.INTER_AREA)
 
                 if self._channels == 3:
-                    roi_seq[current_frame, :] = cv2.cvtColor(mouth_crop, cv2.COLOR_RGB2BGR) / 255
+                    roi_seq[current_frame, :] = cv2.cvtColor(mouth_crop_resized, cv2.COLOR_RGB2BGR) / 255
                 else:
-                    roi_seq[current_frame, :] = np.expand_dims(cv2.cvtColor(mouth_crop, cv2.COLOR_RGB2GRAY) / 255, -1)
+                    roi_seq[current_frame, :] = np.expand_dims(cv2.cvtColor(mouth_crop_resized, cv2.COLOR_RGB2GRAY) / 255, -1)
 
-
-            # Enable these when debugging
+            # # Enable these when debugging # #
             # cv2.imshow('', roi_seq[current_frame, :])
             # cv2.waitKey(1)
 
@@ -157,7 +168,8 @@ class ROIFeature(Feature):
 
     def _write_sequence_to_file(self, file, seq, seq_name, seq_shape):
         import h5py
-        f = h5py.File(self._output_dir+file, 'w')
+
+        f = h5py.File(os.path.join(self._output_dir,file), 'w')
         f.create_dataset(seq_name, data=seq.astype('float32'),
                          maxshape=seq_shape,
                          compression="gzip",
