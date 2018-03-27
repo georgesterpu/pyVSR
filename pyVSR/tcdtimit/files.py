ca91397946706dcf47ffba7aadd51d771f599454
John Tukey <george.sterpu@gmail.com>
2018-03-27 22:20:24 +0100

    lipspeaker support for tcdtimit

--- pyVSR/tcdtimit/files.py
+++ pyVSR/tcdtimit/files.py
@@ -1,5 +1,4 @@
-from os import path, makedirs
-from glob import glob
+from os import path
 from natsort import natsorted
 
 _current_path = path.abspath(path.dirname(__file__))
@@ -21,8 +20,9 @@ volunteers = ('01M', '02M', '03F', '04M', '05F', '06M', '07F', '08F', '09F', '10
               '51F', '52M',        '54M', '55F', '56M', '57M', '58F', '59F',)
 # volunteers 27, 35, 53 excluded for their non-irish accent (e.g. spanish, british)
 
+lipspeakers = ('Lipspkr1', 'Lipspkr2', 'Lipspkr3')
 
-def request_files(dataset_dir, protocol=None, speaker_type=None, gender=None, speaker_id=None, remove_sa=False):
+def request_files(dataset_dir, protocol=None, speaker_type=None, speaker_id=None, remove_sa=False):
     r"""Generates the train/test split according to predefined protocols.
     If no protocol is defined, the function attempts to find all the video files located at `dataset_dir`
     and return a random train/test split.
@@ -33,28 +33,24 @@ def request_files(dataset_dir, protocol=None, speaker_type=None, gender=None, sp
         Can be ``speaker_dependent``, ``speaker_independent``, ``single volunteer``
     speaker_type : 'str' or `None`, optional
         Can be ``volunteer`` or ``lipspeaker``
-    gender : `str`, optional
-        Can be ``females``, ``males`` or ``both``
     speaker_id : `str`, optional
         A three character string encoding the ID of a volunteer, .e.g. ``01M``
     remove_sa : `bool`, optional
         Flag to remove the two `sa` sentences from the file list
         These are spoken by each volunteer
-        
+
     Returns
     -------
 
     """
     if protocol == 'speaker_independent':
-        train, test = _preload_files_speaker_independent(dataset_dir)
+        train, test = _preload_files_speaker_independent(dataset_dir, speaker_type)
     elif protocol == 'speaker_dependent':
         train, test = _preload_files_speaker_dependent(dataset_dir)
     elif protocol == 'single_volunteer':
         train, test = _preload_files_single_volunteer(dataset_dir, speaker_id)
     else:
-        files = _find_files(dataset_dir, speaker_type, gender)
-        from sklearn.model_selection import train_test_split
-        train, test = train_test_split(files, test_size=0.30, random_state=0)
+        raise Exception('unknown protocol')
 
     if remove_sa is True:
         train = [file for file in train if 'sa1' not in file and 'sa2' not in file]
@@ -91,7 +87,7 @@ def _preload_files_speaker_dependent(dataset_dir):
     return train_files, test_files
 
 
-def _preload_files_speaker_independent(dataset_dir):
+def _preload_files_speaker_independent(dataset_dir, speaker_type='volunteer'):
     r"""Speaker-independent protocol
     There are 39 volunteers used for training
     and 17 for testing
@@ -104,9 +100,14 @@ def _preload_files_speaker_independent(dataset_dir):
     -------
 
     """
-
-    train_script = path.join(_current_path, 'splits/speaker-independent/train.scp')
-    test_script = path.join(_current_path, 'splits/speaker-independent/test.scp')
+    if speaker_type == 'volunteer':
+        train_script = path.join(_current_path, 'splits/speaker-independent/volunteers_train.scp')
+        test_script = path.join(_current_path, 'splits/speaker-independent/volunteers_test.scp')
+    elif speaker_type == 'lipspeaker':
+        train_script = path.join(_current_path, 'splits/speaker-independent/lipspeakers_train.scp')
+        test_script = path.join(_current_path, 'splits/speaker-independent/lipspeakers_test.scp')
+    else:
+        raise Exception('Unknown speaker_type')
 
     train_files = [path.join(dataset_dir, line) for line in _read_file_contents(train_script)]
     test_files = [path.join(dataset_dir, line) for line in _read_file_contents(test_script)]
@@ -134,87 +135,6 @@ def _preload_files_single_volunteer(dataset_dir, speaker_id):
     test_files = [path.join(dataset_dir, line) for line in _read_file_contents(test_script) if speaker_id in line]
 
     return train_files, test_files
-
-
-def _find_files(dataset_dir=None, speaker_type='both', gender='both'):
-    r"""Filters the files in the dataset by speaker type and gender
-    Parameters
-    ----------
-    dataset_dir
-    speaker_type
-    gender
-
-    Returns
-    -------
-    files
-
-    """
-    makedirs('./store/', exist_ok=True)
-    contents, status = _read_flist_from_file(speaker_type, gender)
-
-    if status is True:  # list already cached
-        files = contents
-    else:  # create list and cache it for future usage
-        if gender == 'males':
-            g_pattern = 'M'
-        elif gender == 'females':
-            g_pattern = 'F'
-        elif gender == 'both':  # flame on
-            g_pattern = '*'
-        else:
-            raise Exception('Invalid gender parameter')
-
-        if speaker_type == 'volunteers':
-            t_pattern = 'volunteers'
-        elif speaker_type == 'lipspeakers':
-            t_pattern = 'lipspeakers'
-        elif speaker_type == 'both':
-            t_pattern = '*'
-        else:
-            raise Exception('Invalid category parameter')
-
-        pose = 'straightcam'
-        if dataset_dir is None:
-            raise Exception('Path to dataset unspecified')
-
-        if dataset_dir[-1] != '/':
-            dataset_dir += '/'
-
-        files = glob(dataset_dir + t_pattern + '/*' + g_pattern + '/*/' + pose + '/*.mp4')
-
-        files = [file for file in files
-                 if file.find('27M') == -1
-                 and file.find('35M') == -1
-                 and file.find('53M') == -1]
-
-        _write_flist_to_file(files, speaker_type, gender, pose)
-
-    return files
-
-
-def _read_flist_from_file(speaker_type, gender):
-    import pickle
-    files = None
-    status = False
-    pose = 'straightcam'
-
-    fname = speaker_type + '_' + gender + '_' + pose + '.pkl'
-    if path.exists('./store/' + fname):
-        with open('./store/'+fname, 'rb') as f:
-            files = pickle.load(f)
-            status = True
-
-    return files, status
-
-
-def _write_flist_to_file(files, speaker_type, gender, pose):
-    import pickle
-    fname = speaker_type + '_' + gender + '_' + pose + '.pkl'
-    if path. exists('./store/'+fname):
-        pass
-    else:
-        with open('./store/'+fname, 'wb') as f:
-            pickle.dump(files, f)
 
 
 def read_sentence_labels(filename, unit='viseme'):
